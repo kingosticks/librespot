c55dd20bd6c7e44dd75ff33185cf50b2d3bd79c3
Nick Steel <nickasteel@gmail.com>
2018-01-15 22:33:33 +0000

    Tidy up raspberry pi docker script

--- contrib/docker-build-pi-armv6hf.sh
+++ contrib/docker-build-pi-armv6hf.sh
@@ -1,26 +1,34 @@
 #!/usr/bin/env bash
 set -eux
 
-#remove debian armhf installed tools/libs
-apt-get purge -y crossbuild-essential-armhf
-apt-get purge -y *:armhf
+SYSROOT_DIR="/pi-tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/arm-bcm2708hardfp-linux-gnueabi/sysroot"
 
-# get raspberry pi tools (cross compiler etc); use fixed revision for now, should be updated sometimes
-mkdir /pi-tools
-curl -L https://github.com/raspberrypi/tools/archive/648a6eeb1e3c2b40af4eb34d88941ee0edeb3e9a.tar.gz | tar xz --strip-components 1 -C /pi-tools
-export PATH=/pi-tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/:$PATH
-# create wrapper around gcc to point to rpi sysroot
-echo -e '#!/bin/bash' > /pi-tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/gcc-sysroot
-echo -e "arm-linux-gnueabihf-gcc --sysroot /pi-tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/arm-bcm2708hardfp-linux-gnueabi/sysroot \"\$@\"" >> /pi-tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/gcc-sysroot
-chmod +x /pi-tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/gcc-sysroot
-# get alsa lib and headers
-curl -OL http://mirrordirector.raspbian.org/raspbian/pool/main/a/alsa-lib/libasound2_1.0.25-4_armhf.deb
-ar p libasound2_1.0.25-4_armhf.deb data.tar.gz | tar -xz -C /pi-tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/arm-bcm2708hardfp-linux-gnueabi/sysroot
-curl -OL http://mirrordirector.raspbian.org/raspbian/pool/main/a/alsa-lib/libasound2-dev_1.0.25-4_armhf.deb
-ar p libasound2-dev_1.0.25-4_armhf.deb data.tar.gz | tar -xz -C /pi-tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/arm-bcm2708hardfp-linux-gnueabi/sysroot/
+# Add extra target dependencies to our rpi sysroot
+DEPS=( \
+http://mirrordirector.raspbian.org/raspbian/pool/main/a/alsa-lib/libasound2_1.0.25-4_armhf.deb \
+http://mirrordirector.raspbian.org/raspbian/pool/main/a/alsa-lib/libasound2-dev_1.0.25-4_armhf.deb \
+)
+for path in "${DEPS[@]}"; do
+    curl -OL $path
+    ar p $(basename $path) data.tar.gz | tar -xz -C $SYSROOT_DIR
+done
 # i don't why this is neccessary
-ln -s ld-linux.so.3 /pi-tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/arm-bcm2708hardfp-linux-gnueabi/sysroot/lib/ld-linux-armhf.so.3
-# point cargo to use gcc wrapper as linker
-echo -e '[target.arm-unknown-linux-gnueabihf]\nlinker = "gcc-sysroot"' > /.cargo/config
+#ln -s ld-linux.so.3 $SYSROOT_DIR/lib/ld-linux-armhf.so.3
 
+GCC_BIN_DIR="/pi-tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin"
+
+# Some crates want to work this out for themselves and otherwise get it wrong 
+export TARGET_CC="$GCC_BIN_DIR/arm-linux-gnueabihf-gcc"
+
+# create wrapper around gcc to point to rpi sysroot
+GCC_WRAPPER="$GCC_BIN_DIR/gcc-sysroot"
+echo -e '#!/bin/bash' "\n$TARGET_CC --sysroot $SYSROOT_DIR \"\$@\"" > $GCC_WRAPPER
+chmod +x $GCC_WRAPPER
+
+# point cargo to use our gcc wrapper
+echo -e "[target.arm-unknown-linux-gnueabihf]\nlinker = \"$GCC_WRAPPER\"" > /.cargo/config
+
+# Build!
+cd /src
 cargo build --release --target arm-unknown-linux-gnueabihf --no-default-features --features alsa-backend
+
